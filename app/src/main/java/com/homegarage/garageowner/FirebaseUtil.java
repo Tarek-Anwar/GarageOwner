73f648737b8d8523fa715af81b246412f640915e
Tarek <https://github.com/Tarek-Anwar/TourGuideApp>
2022-04-22 19:36:11 +0200

    Revert "pay all"
    This reverts commit 0a5b86e1

--- app/src/main/java/com/homegarage/garageowner/FirebaseUtil.java
+++ app/src/main/java/com/homegarage/garageowner/FirebaseUtil.java
@@ -14,21 +14,28 @@ import java.util.ArrayList;
 
 public class FirebaseUtil {
 
-    public static FirebaseDatabase mFirebaseDatabase;
     public static DatabaseReference mDatabaseReference;
+    public static DatabaseReference referenceOperattion;
+    public static DatabaseReference referenceCar;
+    public static DatabaseReference referencePurchase;
+    public static FirebaseDatabase mFirebaseDatabase;
+
     private static FirebaseUtil mFirebaseUtil;
     public static FirebaseAuth mFirebaseAuthl;
+    public static FirebaseAuth.AuthStateListener mAuthStateListener;
+
     public static InfoUserGarageModel userGarageSign;
     public static StorageReference mStorageReference;
     public static FirebaseStorage mStorage;
-    public static FirebaseAuth.AuthStateListener mAuthStateListener;
-    public static DatabaseReference referenceOperattion;
-    public static InfoUserGarageModel userGarageInfo;
+
 
     public static ArrayList<Opreation> reqstOperaionList;
     public static ArrayList<Opreation> activeOpreations;
+    public static ArrayList<InfoUserGarageModel> userGarageInfo;
+
     public static ArrayList<Integer> stateList ;
     public static ArrayList<Integer> typeList ;
+    public static ArrayList<Integer> paylist;
     public static ArrayList<Opreation> payOpreations;
 
     private FirebaseUtil(){}
@@ -57,16 +64,26 @@ public class FirebaseUtil {
         typeList.add(R.string.requst_type);
         typeList.add(R.string.accpet_type);
         typeList.add(R.string.refusal_type);
-        typeList.add(R.string.pay_type);
+        typeList.add(R.string.cancel);
+        typeList.add(R.string.done);
 
-        userGarageInfo = new InfoUserGarageModel();
+        paylist = new ArrayList<>();
+        paylist.add(R.string.pay_type);
+        paylist.add(R.string.Purchase);
+        paylist.add(R.string.deposit);
+
+
+
         userGarageSign = new InfoUserGarageModel();
 
         activeOpreations=new ArrayList<>();
         payOpreations=new ArrayList<>();
         reqstOperaionList = new ArrayList<>();
+        userGarageInfo = new ArrayList<>();
         mDatabaseReference = mFirebaseDatabase.getReference().child(ref);
         referenceOperattion = mFirebaseDatabase.getReference().child(refOperattion);
+        referenceCar = mFirebaseDatabase.getReference().child("CarInfo");
+        referencePurchase = mFirebaseDatabase.getReference().child("Purchase");
     }
 
     public static void connectStorage(){
